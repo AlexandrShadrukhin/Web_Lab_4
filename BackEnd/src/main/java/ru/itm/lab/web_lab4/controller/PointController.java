6ba7cfb8436647a79452660b62a0ceb1f63778aa
Juju-Enjoyer <115220987+Juju-Enjoyr@users.noreply.github.com>
2024-01-08 02:31:55 +0300

    Add files via upload

--- BackEnd/src/main/java/ru/itm/lab/web_lab4/controller/PointController.java
+++ BackEnd/src/main/java/ru/itm/lab/web_lab4/controller/PointController.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.itm.lab.web_lab4.entity.Point;
-import ru.itm.lab.web_lab4.security.problemResponse;
+import ru.itm.lab.web_lab4.security.errorMessage;
 import ru.itm.lab.web_lab4.service.PointService;
 
 @RestController
@@ -15,34 +15,39 @@ import ru.itm.lab.web_lab4.service.PointService;
 public class PointController {
     private final PointService pointService;
 
-    @GetMapping("/getAll")
+    @GetMapping("/points")
     public ResponseEntity<?> getAllPointsByUserId(@RequestHeader("Authorization") String token) {
         try {
             return ResponseEntity.ok(pointService.getAllPointsById(token));
         } catch (NullPointerException e) {
             return ResponseEntity.ok().build();
+            /*HttpStatus.NOT_FOUND).body(new problemResponse("Any points not found"))*/
         }
     }
 
-    @PostMapping("/create")
+    @PostMapping("/points")
     public ResponseEntity<?> createPoint(@RequestBody Point point, @RequestHeader("Authorization") String token) {
+        if(point==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new errorMessage("Bad Request"));
+        }
         try {
             return ResponseEntity.status(HttpStatus.CREATED).body(pointService.savePoint(token, point));
-        } catch (NullPointerException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new problemResponse("Bad Request"));
         }
         catch (IllegalArgumentException e){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new problemResponse("Incorrect coordination"));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new errorMessage("Incorrect coordination"));
         }
     }
 
-    @DeleteMapping("/delete")
+    @DeleteMapping("/point")
     public ResponseEntity<?> deletePoint(@RequestBody Point point, @RequestHeader("Authorization") String token) {
+        if(point==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Point null");
+        };
         pointService.deletePoint(point);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Deletion successful");
     }
 
-    @DeleteMapping("/deleteAll")
+    @DeleteMapping("/points")
     public ResponseEntity<?> deletePoints(@RequestHeader("Authorization") String token) {
         pointService.deletePoints(token);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Deletion successful");
