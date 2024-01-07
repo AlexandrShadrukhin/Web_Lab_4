b18814c014c2782bbf8390aa79284c5f25b61a65
Juju-Enjoyer <115220987+Juju-Enjoyr@users.noreply.github.com>
2024-01-08 02:41:15 +0300

    Delete FrontEnd directory

--- FrontEnd/src/service/authHeader.js
+++
@@ -1,9 +0,0 @@
-export default function authHeader() {
-    const jwt = JSON.parse(localStorage.getItem('token'));
-
-    if (jwt && jwt.token) {
-        return {Authorization: `Bearer ` + jwt.token};
-    }else{
-        return {};
-    }
-}
