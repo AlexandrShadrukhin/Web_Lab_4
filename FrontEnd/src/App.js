0f3c30b9881ac727cbe8ae292139ab7a761feb3e
Juju-Enjoyer <115220987+Juju-Enjoyr@users.noreply.github.com>
2024-01-08 12:09:01 +0300

    Add files via upload
    [~] provider in app

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 import React, {useCallback} from "react";
 import {connect, Provider, useDispatch, useSelector} from "react-redux";
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import {Helmet} from 'react-helmet';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -8,20 +8,14 @@ import boobs from './components/img/boobs.jpg'
 import Links from './components/Links'
 import Login from "./pages/Login"
 import Register from "./pages/Register";
-import authSlice, {logout} from "./slice/authSlice";
 import Main from "./pages/Main";
 import Info from "./pages/Info";
 import {store} from "./store/store";
-import messageSlice from "./slice/messageSlice";
+
 
 const App = () => {
-    const {token: currentToken} = useSelector((state) => state.auth);
-    const dispatch = useDispatch();
-    const logOut = useCallback(() => {
-        dispatch(logout());
-    }, [dispatch]);
-
     return (
+        <Provider store={store}>
             <Router>
                 <div style={{
                     backgroundImage: `url(${boobs})`,
@@ -32,10 +26,7 @@ const App = () => {
                     <Helmet>
                         <title>Web Lab 4</title>
                     </Helmet>
-                    <Links
-                        currentToken={currentToken}
-                        logOut={logOut}
-                    />
+                    <Links/>
                     <div className="container mt-3">
                         <Routes>
                             <Route path="/info" element={<Info/>}/>
@@ -46,12 +37,13 @@ const App = () => {
                     </div>
                 </div>
             </Router>
+        </Provider>
     );
 };
-const mapStateToProps = store => {
+/*const mapStateToProps = store => {
     return {
         auth: authSlice,
         message: messageSlice
     }
-}
-export default connect(mapStateToProps)(App);
+}*/
+export default App;
